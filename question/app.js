b09f25a49dd2b5d9392ed7cdee851c78e9d04409
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-09-01 17:03:25 +0900

    add:Q3回答

--- question/app.js
+++ question/app.js
@@ -14,3 +14,15 @@ let languages = ['JavaScript', 'PHP', 'Ruby', 'Python','Go'];
 let languagesText = `私の好きな言語は${languages[0]}です。
 次は${languages[3]}を勉強してみたいです。`;
 console.log(languagesText);
+
+// Q3 オブジェクト
+// 下記の変数 user を使用し 26 をコンソールに出力してください。
+
+let user = {
+  name: 'John',
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+};
+
+console.log(user.age);
