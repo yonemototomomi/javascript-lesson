83a6396a4f90babc8568022a25dd7025f4f92884
米本 朋未 <yonemototomomi@yonemotonoMacBook-Air.local>
2025-09-01 12:35:16 +0900

    add:Q1回答

---
+++ question/app.js
@@ -0,0 +1,12 @@
+// Q1 変数
+// 変数 nickname, age に自分の名前と年齢を代入し、文字列連結で以下のようにコンソールに出力してください。
+// 例：私のニックネームはごっしーです。年齢は28歳です。
+let nickname = 'ともみ';
+let age = 28
+let me = '私のニックネームは' + nickname + 'です。年齢は'+ age + '歳です。';
+console.log(me); 
+
+// Q2 配列
+// 変数 languages に JavaScript, PHP, Ruby, Python, Go の 5 つの値を持つ配列を代入し、
+// その中から JavaScript, Python を取得し、テンプレートリテラルを用いて以下のようにコンソールに出力してください。
+// 例：私の好きな言語はJavaScriptです。次はPythonを勉強してみたいです。
